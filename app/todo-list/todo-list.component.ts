a1dd9e8f5cc430e40f5531af624bfcd80018681b
Dmitry Bukavin <djaconda@bitfocus.com>
2017-03-11 14:12:25 +0200

    simple application:
    - create module;
    - create components;
    - directives;
    - data binding;
    - forms;

---
+++ app/todo-list/todo-list.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: 'todo-list',
+    templateUrl: 'todo-list.component.html',
+    styleUrls: ['todo-list.component.css'],
+})
+export class TodoListComponent {
+
+}
